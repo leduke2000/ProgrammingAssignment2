4f7401fc396c73b9cf48c72f0bbcc7d460f72933
Andrey Indu <indukaev.a@gmail.com>
2014-05-26 01:27:27 +0200

    first version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions to calculate mean of a matrix and save in 
+## global environement
 
-## Write a short comment describing this function
+## A function create a list of functions that permit to 
+## set and to get global values of input et output of next function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s<-NULL
+  set<-function(y)
+  {
+    x<<-y
+    s<<-NULL
+  }
+  get<-function() x
+  setsolve<-function(sol) s<<-sol
+  getsolve<-function() s
+  list(set=set,get=get,setsolve=setsolve,getsolve=getsolve)
 }
 
-
-## Write a short comment describing this function
+## A function to get the inverse of a square matrix, 
+## input should of produced by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s<-x$getsolve()
+  if(!is.null(s))
+  {
+    message("gettin cached data")
+    return(s)
+  }
+  data<-x$get()
+  s<-solve(data,...)
+  x$setsolve(s)
+  s
 }
